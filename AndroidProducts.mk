e3f2de487dea4f4ba6448c077d3454521a6497e8
BlackDeath <BlackDeaths@users.noreply.github.com>
2017-12-23 03:58:18 +0100

    add beast

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,4 +16,4 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_angler.mk \
-    $(LOCAL_DIR)/nitrogen_angler.mk
+    $(LOCAL_DIR)/best_angler.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,4 @@
 #
 
 add_lunch_combo aosp_angler-userdebug
+add_lunch_combo beast_angler-userdebug
